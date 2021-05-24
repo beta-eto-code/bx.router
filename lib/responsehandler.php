a4330388fea489dda596c7c274067792f5fe6529
Alexander Nesterov <alex19pov31@gmail.com>
2021-05-24 11:08:48 +0300

    fix(headers) update handle headers in response handler

--- lib/responsehandler.php
+++ lib/responsehandler.php
@@ -12,9 +12,7 @@ class ResponseHandler implements ResponseHandlerInterface
 {
     private function setHeaders(array $headers)
     {
-        foreach ($headers as $item) {
-            $key = $item['name'];
-            $value = $item['values'];
+        foreach ($headers as $key => $value) {
             $value = trim(is_array($value) ? implode(',', $value) : $value);
 
             if (strtolower($key) === 'status') {
@@ -28,11 +26,6 @@ class ResponseHandler implements ResponseHandlerInterface
     public function handle(ResponseInterface $response)
     {
         $this->setHeaders($response->getHeaders());
-        /*$status = implode(' ', [
-            $response->getStatusCode(),
-            $response->getReasonPhrase()
-        ]);*/
-        //header("HTTP/{$response->getProtocolVersion()} {$status}");
         echo $response->getBody();
     }
 }
