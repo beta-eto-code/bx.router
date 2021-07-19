13d6ff0b5b127cfde938a69a32db4c46d13f8cfb
Alexander Nesterov <alex19pov31@gmail.com>
2021-07-19 17:13:54 +0300

    fix(installer) update installer

--- install/index.php
+++ install/index.php
@@ -18,20 +18,59 @@ class bx_router extends CModule
 
     public function __construct()
     {
-        $this->MODULE_VERSION = "0.0.1";
+        $this->MODULE_VERSION = "1.0.1";
         $this->MODULE_VERSION_DATE = "2020-11-28 22:03:00";
         $this->MODULE_NAME = "Роутер";
         $this->MODULE_DESCRIPTION = "Роутер";
     }
 
-    public function DoInstall()
+    /**
+     * @param string $message
+     */
+    public function setError(string $message)
     {
+        $GLOBALS["APPLICATION"]->ThrowException($message);
+    }
+
+    /**
+     * @return bool
+     */
+    public function installRequiredModules(): bool
+    {
+        $isInstalled = ModuleManager::isModuleInstalled('bx.jwt');
+        if ($isInstalled) {
+            return true;
+        }
+
+        $modulePath = getLocalPath("modules/bx.jwt/install/install.php");
+        if (!$modulePath) {
+            $this->setError('Отсутствует модуль bx.jwt - https://github.com/beta-eto-code/bx.jwt');
+            return false;
+        }
+
+        require_once $modulePath;
+        $moduleInstaller = new bx_router();
+        $resultInstall = (bool)$moduleInstaller->DoInstall();
+        if (!$resultInstall) {
+            $this->setError('Ошибка установки модуля bx.jwt');
+        }
+
+        return $resultInstall;
+    }
+
+    public function DoInstall(): bool
+    {
+        $result = $this->installRequiredModules();
+        if (!$result) {
+            return false;
+        }
+
         ModuleManager::RegisterModule($this->MODULE_ID);
         $this->InstallDB();
         return true;
     }
 
-    public function InstallDB()
+    public function InstallDB(): bool
     {
         if(!Loader::includeModule($this->MODULE_ID)){
             return false;
