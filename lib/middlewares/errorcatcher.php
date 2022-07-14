6f0febd7f04078f2575934c26cf5533566512dea
Alexander Nesterov <alex19pov31@gmail.com>
2022-07-14 13:09:32 +0300

    feat(middleware) add error catcher middleware

---
+++ lib/middlewares/errorcatcher.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BX\Router\Middlewares;
+
+use BX\Router\Interfaces\AppFactoryInterface;
+use BX\Router\Interfaces\MiddlewareChainInterface;
+use BX\Router\Middlewares\Traits\ChainHelper;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
+
+class ErrorCatcher implements MiddlewareChainInterface
+{
+    use ChainHelper;
+
+    /**
+     * @var AppFactoryInterface
+     */
+    private AppFactoryInterface $factory;
+
+    public function __construct(AppFactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            return $this->runChain($request, $handler);
+        } catch (Throwable $e) {
+            return $this->factory->createJsonResponse([
+                'error' => true,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+}
