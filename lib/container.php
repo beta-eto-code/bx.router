2ee0342f7841f8258cbba1db5fda0b62b0c5fb88
Alexander Nesterov <alex19pov31@gmail.com>
2022-09-19 17:13:01 +0300

    fix(container) update get method

--- lib/container.php
+++ lib/container.php
@@ -1,13 +1,13 @@
 <?php
 
-
 namespace BX\Router;
-
 
 use Bitrix\Main\DI\ServiceLocator;
 use Bitrix\Main\ObjectNotFoundException;
 use BX\Router\Interfaces\ContainerGetterInterface;
 use BX\Router\Interfaces\ContainerSetterInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Throwable;
 
 class Container implements ContainerSetterInterface, ContainerGetterInterface
 {
@@ -24,18 +24,37 @@ class Container implements ContainerSetterInterface, ContainerGetterInterface
     /**
      * @param string $name
      * @return mixed
-     * @throws ObjectNotFoundException
+     * @throws ObjectNotFoundException|NotFoundExceptionInterface
      */
     public function get(string $name)
     {
-        return $this->container->get($name);
+        $result = $this->container->get($name);
+        if (is_callable($result)) {
+            try {
+                $result = $result();
+                $this->set($name, $result);
+            } catch (Throwable $e) {
+                return null;
+            }
+        }
+
+        return $result;
     }
 
+    /**
+     * @param string $name
+     * @return bool
+     */
     public function has(string $name): bool
     {
         return $this->container->has($name);
     }
 
+    /**
+     * @param string $name
+     * @param $serviceInstance
+     * @return void
+     */
     public function set(string $name, $serviceInstance)
     {
         $this->container->addInstance($name, $serviceInstance);
