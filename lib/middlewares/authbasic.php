132bb60fb4c012264ddb88611ed020190080d25b
Nesterov Alexandr <alex19pov31@gmail.com>
2023-10-24 13:49:10 +0300

    feat(basic auth): update basic auth - init required auth mode

--- lib/middlewares/authbasic.php
+++ lib/middlewares/authbasic.php
@@ -5,6 +5,7 @@ namespace BX\Router\Middlewares;
 use Bx\Model\Interfaces\AccessStrategyInterface;
 use Bx\Model\Interfaces\UserContextInterface;
 use Bx\Model\Interfaces\UserServiceInterface;
+use BX\Router\Interfaces\AppFactoryInterface;
 use BX\Router\Interfaces\MiddlewareChainInterface;
 use BX\Router\Middlewares\Traits\ChainHelper;
 use Psr\Http\Message\ResponseInterface;
@@ -23,16 +24,38 @@ class AuthBasic implements MiddlewareChainInterface
      * @var ?AccessStrategyInterface
      */
     private $accessStrategy;
+    /**
+     * @var bool
+     */
+    private $authIsRequired;
+
+    /**
+     * @var AppFactoryInterface|null
+     */
+    private $appFactory = null;
+
+    /**
+     * @var string|null
+     */
+    private $notificationMessage = null;
 
     /**
      * AuthBasic constructor.
      * @param UserServiceInterface $userService
      * @param AccessStrategyInterface|null $accessStrategy
+     * @param bool $authIsRequired
      */
-    public function __construct(UserServiceInterface $userService, ?AccessStrategyInterface $accessStrategy = null)
-    {
+    public function __construct(UserServiceInterface $userService, ?AccessStrategyInterface $accessStrategy = null) {
+        $this->authIsRequired = false;
         $this->userService = $userService;
         $this->accessStrategy = $accessStrategy;
+    }
+
+    public function setRequiredAuthMode(AppFactoryInterface $appFactory, ?string $notificationMessage = null): void
+    {
+        $this->authIsRequired = true;
+        $this->appFactory = $appFactory;
+        $this->notificationMessage = $notificationMessage;
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
@@ -42,6 +65,13 @@ class AuthBasic implements MiddlewareChainInterface
         $password = (string)$params['PHP_AUTH_PW'];
 
         $userContext = $this->userService->login($user, $password);
+        if ($this->authIsRequired && empty($userContext) && $this->appFactory instanceof AppFactoryInterface) {
+            $response = $this->appFactory->createJsonResponse([
+                'message' => $this->notificationMessage ?: 'Необходимо авторизоваться'
+            ], 401);
+            return $response->withHeader('WWW-Authenticate', 'Basic');
+        }
+
         if ($userContext instanceof UserContextInterface && $this->accessStrategy instanceof AccessStrategyInterface) {
             $userContext->setAccessStrategy($this->accessStrategy);
         }
